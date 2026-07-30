ad8076ecea60edde4436e506612942f4dd26b622
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-07-30 13:20:30 -0400

    Adiciona funcionamento básico offline

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'transporte-heuro-v1';
+const APP_FILES = ['./', './index.html', './style.css', './app.js', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
