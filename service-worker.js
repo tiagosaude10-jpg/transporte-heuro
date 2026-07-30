24c2d3ebf744034d1dab3edac4b0a2bcc5a3a2c7
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-07-30 15:18:25 -0400

    Atualizar cache após correção do setor

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='transporte-heuro-v3';
+const CACHE_NAME='transporte-heuro-v4';
 const APP_FILES=['./','./index.html','./style.css','./app.js','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim();});
